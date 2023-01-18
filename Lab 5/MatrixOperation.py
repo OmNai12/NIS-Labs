c9d2b8049a66dc0db74c0366101e25e0294c1a5c
Om Nai <93648778+OmNai12@users.noreply.github.com>
2023-01-19 01:11:23 +0530

    Bug Fix in hill cipher

---
+++ Lab 5/MatrixOperation.py
@@ -0,0 +1,11 @@
+def matrixMultiplication(mat1, mat2):
+    # mat1 :- Plain Text Matrix
+    # mat2 :- Key Matrix
+
+    pass
+
+
+if __name__ == "__main__":
+    m1 = [[1, 2], [3, 4]]
+    m2 = [[7, 5], [4, 2]]
+    matrixMultiplication(m1, m2)
